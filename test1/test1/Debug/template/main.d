0c7d602ee41229293152d66a076ee45eb93089b8
Sergey Novikov <nsv47nsv36@yandex.ru>
2024-12-08 18:21:26 +0300

    рабочий проект для T-FPGA. Мигет светодиодом

---
+++ test1/test1/Debug/template/main.d
@@ -0,0 +1,40 @@
+template/main.o: ../template/main.c \
+ C:\GMD\workspace\test1\lib\CMSIS\DeviceSupport\system/gw1ns4c.h \
+ C:\GMD\workspace\test1\lib\CMSIS\CoreSupport\gmd/core_cm3.h \
+ C:\GMD\workspace\test1\lib\CMSIS\DeviceSupport\system/system_gw1ns4c.h \
+ C:\GMD\workspace\test1\template/gw1ns4c_conf.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_gpio.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_wdog.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_uart.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_timer.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_spi.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_i2c.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_misc.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_syscon.h \
+ C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_rtc.h
+
+C:\GMD\workspace\test1\lib\CMSIS\DeviceSupport\system/gw1ns4c.h:
+
+C:\GMD\workspace\test1\lib\CMSIS\CoreSupport\gmd/core_cm3.h:
+
+C:\GMD\workspace\test1\lib\CMSIS\DeviceSupport\system/system_gw1ns4c.h:
+
+C:\GMD\workspace\test1\template/gw1ns4c_conf.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_gpio.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_wdog.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_uart.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_timer.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_spi.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_i2c.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_misc.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_syscon.h:
+
+C:\GMD\workspace\test1\lib\StdPeriph_Driver\Includes/gw1ns4c_rtc.h:
