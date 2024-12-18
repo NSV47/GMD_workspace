4298f8903171ec2595faa255ff01bedc0693531b
Sergey Novikov <nsv47nsv36@yandex.ru>
2024-12-18 18:14:29 +0300

    удалил из отслеживания ненужные файлы

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/86/203cc32332b7001f1a70ba0874dfe5f6
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#if 1
+struct Gen{
+	uint32_t freq;
+	uint32_t phase;
+};
+#endif
+
+#endif /* MAIN_H */
---
+++ test1/test1/Debug/template/hfbeacon.d
@@ -0,0 +1,4 @@
+template/hfbeacon.o: ../template/hfbeacon.c \
+ C:\GMD\workspace\test1\test1\template/hfbeacon.h
+
+C:\GMD\workspace\test1\test1\template/hfbeacon.h:
